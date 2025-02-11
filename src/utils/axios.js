4c2d130df8e697854a91b0a0e95a0bcce1e8c1ce
AkramAn2002 <126903833+AkramAn2002@users.noreply.github.com>
2025-02-11 14:45:42 +0100

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "http://localhost:8087",
+  baseURL: "https://gsincident.onrender.com/",
 });
 
 export default customFetch;
